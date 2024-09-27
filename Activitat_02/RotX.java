96e4e64a5e4ab434968d4a7e8778d677d7cd91b8
Lishi Ji Liu <2023_li-shi.ji@iticbcn.cat>
2024-09-27 16:09:35 +0200

    funcional

--- Activitat_02/RotX.java
+++ Activitat_02/RotX.java
@@ -1,10 +1,12 @@
-/* Ara el programa enlloc de moure 13 posicions, pots introduir un paràmetre 
+/* Ara el programa enlloc de moure 13 posicions, pots introduïr un paràmetre 
  * extra per a una posició diferent, ficarem 4 posicions possibles pel testing
- * i a més de contenir els dos mètodes xifraRotX i desxifratRotX, hi haurà
+ * i a més de contenir els dos mètodes xifraRotX i desxifraRotX, hi haurà
  * un tercer que farà el mateix que desxifraRotX però forcaBrutaRotX ho farà
  * per força bruta, en que mira i mostra per totes les posicions possibles, en
  * que les possibilitats són la longitud de l'abecedari. El main contindrà totes
- * les proves */
+ * les proves.
+ * 
+ * DAM2B: Lishi JL */
 
 public class RotX {
     public static final char[] LOWERCHARS = "aàábcçdeèéfghiìíïjklmnñoòópqrstuùúüvwxyz".toCharArray();
@@ -14,24 +16,77 @@ public class RotX {
         int[] positions = { 2, 6, 3, 10 };
 
         for (String test : tests) {
-            System.out.println("Original: " +  + " -> Encriptat amb " +  + " posicions: " + );
-            System.out.println("Xifrat: " +  + " -> Desxifrat també amb " +  + " posicions: " + );
-            System.out.println("Ara amb força bruta: " + forcaBrutaRotX(/* passem el resultat de xifra(s,x) */));
+            for (int position : positions) {
+                String xifrat = xifraRotX(test, position);
+                String desxifrat = desxifraRotX(xifrat, position);
+                System.out.println("Original: " + test + " -> Encriptat amb " + position + " posicions: " + xifrat);
+                System.out.println("Xifrat: " + xifrat + " -> Desxifrat també amb " + position + " posicions: " + desxifrat);
+                System.out.println("Ara desxifrem \"" + xifrat + "\" amb força bruta:\n" + forcaBrutaRotX(xifrat));
+            }
         }
     }
-    public static String xifraRotX(String paraula, int postion) {
-
+    public static String xifraRotX(String paraula, int position) {
+        StringBuffer result = new StringBuffer();
+        for (int i = 0; i < paraula.length(); i++) {
+            char originalCh = paraula.charAt(i);
+            int posMin = trobaPosicio(LOWERCHARS, originalCh);
+            int posMay = trobaPosicio(UPPERCHARS, originalCh);
+            int finalPos;
+            if (posMin > -1) {
+                finalPos = (posMin + position) % LOWERCHARS.length;
+                result.append(LOWERCHARS[finalPos]);
+            } else if (posMay > -1) {
+                finalPos = (posMay + position) % UPPERCHARS.length;
+                result.append(UPPERCHARS[finalPos]);
+            } else {
+                result.append(originalCh);
+            }
+        }
+        return result.toString();
     }
     public static String desxifraRotX(String xifrat, int position) {
-
+        StringBuffer result = new StringBuffer();
+        for (int i = 0; i < xifrat.length(); i++) {
+            char originalCh = xifrat.charAt(i);
+            int posMin = trobaPosicio(LOWERCHARS, originalCh);
+            int posMay = trobaPosicio(UPPERCHARS, originalCh);
+            int resultPos;
+            int finalPos;
+            if (posMin > -1) {
+                resultPos = (posMin - position) % LOWERCHARS.length;
+                if (resultPos < 0) {
+                    finalPos = LOWERCHARS.length + resultPos;
+                    result.append(LOWERCHARS[finalPos]);
+                } else {
+                    result.append(LOWERCHARS[resultPos]);
+                }
+            } else if (posMay > -1) {
+                resultPos = (posMay - position) % UPPERCHARS.length;
+                if (resultPos < 0) {
+                    finalPos = UPPERCHARS.length + resultPos;
+                    result.append(UPPERCHARS[finalPos]);
+                } else {
+                    result.append(UPPERCHARS[resultPos]);
+                }
+            } else {
+                result.append(originalCh);
+            }
+        }
+        return result.toString();
     }
     public static String forcaBrutaRotX(String xifrat) {
-        // contindrà el resultat sencer a retornar
         StringBuffer result = new StringBuffer();
-        // all positions
         for (int i = 0; i < LOWERCHARS.length; i++) {
-            result.append(desxifraRotX(xifrat, i));
+            result.append("Posició -" + i + ": " + desxifraRotX(xifrat, i) + "\n");
         }
-        return result;
+        return result.toString();
+    }
+    public static int trobaPosicio(char[] array, char character) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == character) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
