3745b196acd45a3548ef21881f72fee52f5247b5
Lishi Ji Liu <2023_li-shi.ji@iticbcn.cat>
2024-10-19 14:27:22 +0200

    main, global var, required methods defined

--- 05-XifratAES/AES.java
+++ 05-XifratAES/AES.java
@@ -18,20 +18,79 @@
  * generat a partir d'una clau i el missatge. */
 
 public class AES {
-    public static void main(String[] args) {
-        String msgs[] = { "Lorem ipsum dicet", "Hola Andrés cómo está tu cuñado", "Àgora ïlla Ôtto" };
-        for (int i = 0; i < msgs.length; i++) {
-            String msg = msgs[i];
+    // variables o definicions globals a nivell classe
+    // Constants, tipus xifrat, tipus hash, tipus format
+    public static final String ALGORISME_XIFRAT = "AES";
+    public static final String ALGORISME_HASH = "SHA-256";
+    public static final String FORMAT_AES = "AES/CBC/PKCS5Padding";
+        
+    // constant mida IV, array de byte iv de tamany 16 i constant de clau
+    private static final int MIDA_IV = 16;
+    private static byte[] iv = new byte[MIDA_IV];
+    private static final String CLAU = "LaClauSecretaQueVulguis";
 
+    // retorna el xifrat en array de bytes a partir d'una clau i un missatge,
+    // mètode que pot generar una excepció, llançable de tipus general
+    public static byte[] xifraAES(String msg, String clau) throws Exception {
+        //Obtenir els bytes de l’String
+
+        // Genera IvParameterSpec
+
+        // Genera hash
+
+        // Encrypt.
+
+        // Combinar IV i part xifrada.
+
+        // return iv+msgxifrat
+        return null;
+    }
+
+    // retorna el desxifrat en String a partir del array de bytes xifrat amb
+    // iv i missatge, i la clau, el mètode pot generar una excepció general
+    // que serà llançable
+    public static String desxifraAES(byte[] bIvIMsgXifrat, String clau) throws Exception {
+
+        // Extreure l'IV.
+
+        // Extreure la part xifrada.
+
+        // Fer hash de la clau
+
+        // Desxifrar.
+
+        // return String desxifrat
+        return "";
+    }
+
+    // main que executa la lógica del programa
+    public static void main(String[] args) {
+        // proves
+        String msgs[] = { "Lorem ipsum dicet", "Hola Andrés cómo está tu cuñado", "Àgora ïlla Ôtto" };
+        // iteració per cada prova
+        for (int i = 0; i < msgs.length; i++) {
+            // obtenció d'1 text per provar
+            String msg = msgs[i];
+            
+            // array de bytes que s¡obtindrà del xifrat
             byte[] bXifrats = null;
+            // resultat de tornar del desxifrat
             String desxifrat = "";
+
+            // gestionem possibles excepcions
             try {
                 bXifrats = xifraAES(msg, CLAU);
                 desxifrat = desxifraAES(bXifrats, CLAU);
             } catch (Exception e) {
+                // mostrem i imprimim l'error capturat, i obtenim la localització del missatge
                 System.err.println("Error de xifrat: " + e.getLocalizedMessage());
             }
-            System.out.print
+            // mostrem la sortida, el missatge original, l'encriptat de tornar l'array de bytes 
+            // en una nova instància de tipus String amb el seu builder i també el missatge desxifrat
+            System.out.println("--------------------");
+            System.out.println("Msg: " + msg);
+            System.out.println("Enc: " + new String(bXifrats));
+            System.out.println(desxifrat);
         }
     }
 }
