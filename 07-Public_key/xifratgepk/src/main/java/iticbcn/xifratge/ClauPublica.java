36e3e4b26b8f36ced5f119fba4d5a524d1cbc8f7
Lishi Ji Liu <2023_li-shi.ji@iticbcn.cat>
2024-11-07 18:41:14 +0100

    classe ClauPublica inicialitzada

---
+++ 07-Public_key/xifratgepk/src/main/java/iticbcn/xifratge/ClauPublica.java
@@ -0,0 +1,18 @@
+package iticbcn.xifratge;
+
+import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+// longitud de clau 2048 bits
+public class ClauPublica {
+    public KeyPair generaParellClausRSA() throws Exception {
+        return null;
+    }
+    public byte[] xifraRSA(String msg, PublicKey clauPublica) throws Exception {
+        return null;
+    }
+    public String desxifraRSA(byte[] msgXifrat, PrivateKey clauPrivada) throws Exception {
+        return null;
+    }
+}
--- 07-Public_key/xifratgepk/src/main/java/iticbcn/xifratge/Main.java
+++ 07-Public_key/xifratgepk/src/main/java/iticbcn/xifratge/Main.java
@@ -1,7 +1,29 @@
 package iticbcn.xifratge;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+/* Classe Main que executarà tota la lógica del programa */
+import java.security.KeyPair; 
+// la llibreria importada ja està implementada en el pom.xml
+import javax.xml.bind.DatatypeConverter; 
+
+public class Main { 
+    public static void main(String args[]) throws Exception { 
+        ClauPublica cp = new ClauPublica(); 
+        String msg = "Missatge de prova per xifrar áéíóú àèìòù äëïöü"; 
+        KeyPair parellClaus = cp.generaParellClausRSA(); 
+        byte[] msgXifrat = cp.xifraRSA(msg, parellClaus.getPublic()); 
+        System.out.println("================================="); 
+        System.out.print("Text xifrat: "); 
+        System.out.println(DatatypeConverter.printHexBinary(msgXifrat)); 
+        String msgDesxifrat = cp.desxifraRSA(msgXifrat, parellClaus.getPrivate()); 
+        System.out.println("================================="); 
+        System.out.println("Text desxifrat: " + msgDesxifrat); 
+        String strClauPub = DatatypeConverter.printHexBinary( 
+        parellClaus.getPublic().getEncoded()); 
+        String strClauPriv = DatatypeConverter.printHexBinary( 
+        parellClaus.getPrivate().getEncoded()); 
+        System.out.println("================================="); 
+        System.out.println("Clau pública: " + strClauPub); 
+        System.out.println("================================="); 
+        System.out.println("Clau privada: " + strClauPriv); 
+    } 
 }
