ff81a5725043bbdea819a4ffaaae2a823fe459f9
Lishi Ji Liu <2023_li-shi.ji@iticbcn.cat>
2024-11-09 12:27:23 +0100

    generació de claus rsa generaParellClausRSA() completat

--- 07-Public_key/xifratgepk/src/main/java/iticbcn/xifratge/ClauPublica.java
+++ 07-Public_key/xifratgepk/src/main/java/iticbcn/xifratge/ClauPublica.java
@@ -1,13 +1,21 @@
 package iticbcn.xifratge;
 
 import java.security.KeyPair;
+import java.security.KeyPairGenerator;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 
+
 // longitud de clau 2048 bits
 public class ClauPublica {
+    // genera parell de claus RSA, la publica per encriptar i la privada per desencriptar
     public KeyPair generaParellClausRSA() throws Exception {
-        return null;
+        // obté una instància d'un generador de claus tipus RSA
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+        // tamany de la clau de 2048 bits
+        keyGen.initialize(2048);
+        // retorna directament la parella de claus generades amb el mètode generateKeyPair() de KeyPairGenerator
+        return keyGen.generateKeyPair();
     }
     public byte[] xifraRSA(String msg, PublicKey clauPublica) throws Exception {
         return null;
