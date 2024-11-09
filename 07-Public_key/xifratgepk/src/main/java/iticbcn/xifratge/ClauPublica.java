5be9755bc3fde867228fae5d8f4da75e051158e3
Lishi Ji Liu <2023_li-shi.ji@iticbcn.cat>
2024-11-09 19:00:48 +0100

    mètodes finalitzats xifraRSA() i desxifraRSA(), generació d'un JAR i resultat de l'execució en el README.md

--- 07-Public_key/xifratgepk/src/main/java/iticbcn/xifratge/ClauPublica.java
+++ 07-Public_key/xifratgepk/src/main/java/iticbcn/xifratge/ClauPublica.java
@@ -1,12 +1,15 @@
 package iticbcn.xifratge;
 
+// imports principals de la classe actual
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 
+// imports pel xifratge amb clau RSA
+import javax.crypto.Cipher;
+import java.nio.charset.StandardCharsets;
 
-// longitud de clau 2048 bits
 public class ClauPublica {
     // genera parell de claus RSA, la publica per encriptar i la privada per desencriptar
     public KeyPair generaParellClausRSA() throws Exception {
@@ -17,10 +20,25 @@ public class ClauPublica {
         // retorna directament la parella de claus generades amb el mètode generateKeyPair() de KeyPairGenerator
         return keyGen.generateKeyPair();
     }
+    // els paràmetres de les claus rebudes son a partir dels getters de la parella de claus que hem retornat en el bloc anterior
+    // xifratge del msg amb clau publica
     public byte[] xifraRSA(String msg, PublicKey clauPublica) throws Exception {
-        return null;
+        // instància amb xifratge d'un mode i tamany que acabarà d'omplir de tipus RSA
+        Cipher cipher = Cipher.getInstance("RSA");
+        // arguments de xifratge mode i la clau publica per xifrar
+        cipher.init(Cipher.ENCRYPT_MODE, clauPublica);
+        // passem el msg String a array de byte amb el charset UTF-8 estandaritzat
+        byte[] msgB = msg.getBytes(StandardCharsets.UTF_8);
+        // retornem el missatge xifrat executant el mètode doFinal()
+        return cipher.doFinal(msgB);
     }
+    // desxifratge amb clau privada
     public String desxifraRSA(byte[] msgXifrat, PrivateKey clauPrivada) throws Exception {
-        return null;
+        Cipher cipher = Cipher.getInstance("RSA");
+        // ara en mode desencriptar i amb clau privada
+        cipher.init(Cipher.DECRYPT_MODE, clauPrivada);
+        // retornem l'array de byte desxifrat del array de byte de msgXifrat, embolicat a un now String() 
+        // amb l'extra del standard charset UTF-8 per assegurar-nos la reconversió entre array de byte a String
+        return new String(cipher.doFinal(msgXifrat), StandardCharsets.UTF_8);
     }
 }
--- 07-Public_key/xifratgepk/src/main/java/iticbcn/xifratge/Main.java
+++ 07-Public_key/xifratgepk/src/main/java/iticbcn/xifratge/Main.java
@@ -7,18 +7,25 @@ import javax.xml.bind.DatatypeConverter;
 
 public class Main { 
     public static void main(String args[]) throws Exception { 
+        // instància de la classe ClauPublica
         ClauPublica cp = new ClauPublica(); 
         String msg = "Missatge de prova per xifrar áéíóú àèìòù äëïöü"; 
+        // obtenció de la parella de claus rsa
         KeyPair parellClaus = cp.generaParellClausRSA(); 
+        // array de bytes del msg xifrat
         byte[] msgXifrat = cp.xifraRSA(msg, parellClaus.getPublic()); 
         System.out.println("================================="); 
         System.out.print("Text xifrat: "); 
+        // converteix el text xifrat d'array de bytes en String que representa hexadecimalment el missatge xifrat
         System.out.println(DatatypeConverter.printHexBinary(msgXifrat)); 
+        // text desxifrat a partir de l'array de bytes del msg xifrat
         String msgDesxifrat = cp.desxifraRSA(msgXifrat, parellClaus.getPrivate()); 
         System.out.println("================================="); 
         System.out.println("Text desxifrat: " + msgDesxifrat); 
+        // converteix la clau publica en un String en base 64 o hexadecimal
         String strClauPub = DatatypeConverter.printHexBinary( 
         parellClaus.getPublic().getEncoded()); 
+        // converteix la clau privada en un String en base 64 o hexadecimal
         String strClauPriv = DatatypeConverter.printHexBinary( 
         parellClaus.getPrivate().getEncoded()); 
         System.out.println("================================="); 
